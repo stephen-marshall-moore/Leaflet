aa4b43a2570e1f90afcccd0afd1d6ac6d460ec3e
Stephen Moore <stephen.moore@linux.com>
2016-02-17 16:18:26 -0800

    geo/*specs pass, ok on phantomjs

--- spec/suites/geo/ProjectionSpec.js
+++ spec/suites/geo/ProjectionSpec.js
@@ -1,96 +1,99 @@
-describe("Projection.Mercator", function () {
-	var p = L.Projection.Mercator;
+import { Point } from '../../../src/geometry/Point';
+import { LatLng } from '../../../src/geo/LatLng';
+import { Mercator } from '../../../src/geo/projection/Projection.Mercator';
+import { SphericalMercator } from '../../../src/geo/projection/Projection.SphericalMercator';
 
-	describe("#project", function () {
-		it("projects a center point", function () {
+describe("Projection.Mercator", () => {
+	var p = new Mercator();
+
+	describe("#project", () => {
+		it("projects a center point", () => {
 			// edge cases
-			expect(p.project(new L.LatLng(0, 0))).near(new L.Point(0, 0));
+			expect(p.project(new LatLng(0, 0)).distanceTo(new Point(0, 0))).toBeLessThan(0.01);
 		});
 
-		it("projects the northeast corner of the world", function () {
-			expect(p.project(new L.LatLng(85.0840591556, 180))).near(new L.Point(20037508, 20037508));
+		it("projects the northeast corner of the world", () => {
+			expect(p.project(new LatLng(85.0840591556, 180)).distanceTo(new Point(20037508, 20037508))).toBeLessThan(1);
 		});
 
-		it("projects the southwest corner of the world", function () {
-			expect(p.project(new L.LatLng(-85.0840591556, -180))).near(new L.Point(-20037508, -20037508));
+		it("projects the southwest corner of the world", () => {
+			expect(p.project(new LatLng(-85.0840591556, -180)).distanceTo(new Point(-20037508, -20037508))).toBeLessThan(1);
 		});
 
-		it("projects other points", function () {
-			expect(p.project(new L.LatLng(50, 30))).near(new L.Point(3339584, 6413524));
+		it("projects other points", () => {
+			expect(p.project(new LatLng(50, 30)).distanceTo(new Point(3339584, 6413524))).toBeLessThan(1);
 
 			// from https://github.com/Leaflet/Leaflet/issues/1578
-			expect(p.project(new L.LatLng(51.9371170300465, 80.11230468750001)))
-			        .near(new L.Point(8918060.964088084, 6755099.410887127));
+			expect(p.project(new LatLng(51.9371170300465, 80.11230468750001)).distanceTo(new Point(8918060.964088084, 6755099.410887127))).toBeLessThan(0.01);
 		});
 	});
 
-	describe("#unproject", function () {
+	describe("#unproject", () => {
 		function pr(point) {
 			return p.project(p.unproject(point));
 		}
 
-		it("unprojects a center point", function () {
-			expect(pr(new L.Point(0, 0))).near(new L.Point(0, 0));
+		it("unprojects a center point", () => {
+			expect(pr(new Point(0, 0)).distanceTo(new Point(0, 0))).toBeLessThan(0.01);
 		});
 
-		it("unprojects pi points", function () {
-			expect(pr(new L.Point(-Math.PI, Math.PI))).near(new L.Point(-Math.PI, Math.PI));
-			expect(pr(new L.Point(-Math.PI, -Math.PI))).near(new L.Point(-Math.PI, -Math.PI));
+		it("unprojects pi points", () => {
+			expect(pr(new Point(-Math.PI, Math.PI)).distanceTo(new Point(-Math.PI, Math.PI))).toBeLessThan(0.01);
+			expect(pr(new Point(-Math.PI, -Math.PI)).distanceTo(new Point(-Math.PI, -Math.PI))).toBeLessThan(0.01);
 
-			expect(pr(new L.Point(0.523598775598, 1.010683188683))).near(new L.Point(0.523598775598, 1.010683188683));
+			expect(pr(new Point(0.523598775598, 1.010683188683)).distanceTo(new Point(0.523598775598, 1.010683188683))).toBeLessThan(0.01);
 		});
 
-		it('unprojects other points', function () {
+		it('unprojects other points', () => {
 			// from https://github.com/Leaflet/Leaflet/issues/1578
-			expect(pr(new L.Point(8918060.964088084, 6755099.410887127)));
+			expect(pr(new Point(8918060.964088084, 6755099.410887127)));
 		});
 	});
 });
-describe("Projection.SphericalMercator", function () {
-	var p = L.Projection.SphericalMercator;
+describe("Projection.SphericalMercator", () => {
+	var p = new SphericalMercator();
 
-	describe("#project", function () {
-		it("projects a center point", function () {
+	describe("#project", () => {
+		it("projects a center point", () => {
 			// edge cases
-			expect(p.project(new L.LatLng(0, 0))).near(new L.Point(0, 0));
+			expect(p.project(new LatLng(0, 0)).distanceTo(new Point(0, 0))).toBeLessThan(0.01);
 		});
 
-		it("projects the northeast corner of the world", function () {
-			expect(p.project(new L.LatLng(85.0511287798, 180))).near(new L.Point(20037508, 20037508));
+		it("projects the northeast corner of the world", () => {
+			expect(p.project(new LatLng(85.0511287798, 180)).distanceTo(new Point(20037508, 20037508))).toBeLessThan(0.5);
 		});
 
-		it("projects the southwest corner of the world", function () {
-			expect(p.project(new L.LatLng(-85.0511287798, -180))).near(new L.Point(-20037508, -20037508));
+		it("projects the southwest corner of the world", () => {
+			expect(p.project(new LatLng(-85.0511287798, -180)).distanceTo(new Point(-20037508, -20037508))).toBeLessThan(0.5);
 		});
 
-		it("projects other points", function () {
-			expect(p.project(new L.LatLng(50, 30))).near(new L.Point(3339584, 6446275));
+		it("projects other points", () => {
+			expect(p.project(new LatLng(50, 30)).distanceTo(new Point(3339584, 6446275))).toBeLessThan(1.2);
 
 			// from https://github.com/Leaflet/Leaflet/issues/1578
-			expect(p.project(new L.LatLng(51.9371170300465, 80.11230468750001)))
-					.near(new L.Point(8918060.96409, 6788763.38325));
+			expect(p.project(new LatLng(51.9371170300465, 80.11230468750001)).distanceTo(new Point(8918060.96409, 6788763.38325))).toBeLessThan(0.01);
 		});
 	});
 
-	describe("#unproject", function () {
+	describe("#unproject", () => {
 		function pr(point) {
 			return p.project(p.unproject(point));
 		}
 
-		it("unprojects a center point", function () {
-			expect(pr(new L.Point(0, 0))).near(new L.Point(0, 0));
+		it("unprojects a center point", () => {
+			expect(pr(new Point(0, 0)).distanceTo(new Point(0, 0))).toBeLessThan(0.01);
 		});
 
-		it("unprojects pi points", function () {
-			expect(pr(new L.Point(-Math.PI, Math.PI))).near(new L.Point(-Math.PI, Math.PI));
-			expect(pr(new L.Point(-Math.PI, -Math.PI))).near(new L.Point(-Math.PI, -Math.PI));
+		it("unprojects pi points", () => {
+			expect(pr(new Point(-Math.PI, Math.PI)).distanceTo(new Point(-Math.PI, Math.PI))).toBeLessThan(0.01);
+			expect(pr(new Point(-Math.PI, -Math.PI)).distanceTo(new Point(-Math.PI, -Math.PI))).toBeLessThan(0.01);
 
-			expect(pr(new L.Point(0.523598775598, 1.010683188683))).near(new L.Point(0.523598775598, 1.010683188683));
+			expect(pr(new Point(0.523598775598, 1.010683188683)).distanceTo(new Point(0.523598775598, 1.010683188683))).toBeLessThan(0.01);
 		});
 
-		it('unprojects other points', function () {
+		it('unprojects other points', () => {
 			// from https://github.com/Leaflet/Leaflet/issues/1578
-			expect(pr(new L.Point(8918060.964088084, 6755099.410887127)));
+			expect(pr(new Point(8918060.964088084, 6755099.410887127)));
 		});
 	});
 });
