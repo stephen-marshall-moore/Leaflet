f1a36434a97b2ffce36fa8adafab24d2b897d20d
Stephen Moore <stephen.moore@linux.com>
2016-03-13 18:37:35 -0700

    Browser class seems successful

--- spec/suites/dom/DomEventSpec.js
+++ spec/suites/dom/DomEventSpec.js
@@ -1,102 +1,106 @@
+"use strict"
+
+import {DomEvent} from 'src/dom/DomEvent'
+
 describe('DomEvent', function () {
-	var el;
+	let el
 
 	function simulateClick(el) {
 		if (document.createEvent) {
-			var e = document.createEvent('MouseEvents');
+			let e = document.createEvent('MouseEvents')
 			e.initMouseEvent('click', true, true, window,
-					0, 0, 0, 0, 0, false, false, false, false, 0, null);
-			return el.dispatchEvent(e);
+					0, 0, 0, 0, 0, false, false, false, false, 0, null)
+			return el.dispatchEvent(e)
 		} else if (el.fireEvent) {
-			return el.fireEvent('onclick');
+			return el.fireEvent('onclick')
 		}
 	}
 
 	beforeEach(function () {
-		el = document.createElement('div');
-		el.style.position = 'absolute';
-		el.style.top = el.style.left = '-10000px';
-		document.body.appendChild(el);
-	});
+		el = document.createElement('div')
+		el.style.position = 'absolute'
+		el.style.top = el.style.left = '-10000px'
+		document.body.appendChild(el)
+	})
 
 	afterEach(function () {
-		document.body.removeChild(el);
-	});
+		document.body.removeChild(el)
+	})
 
 	describe('#addListener', function () {
 		it('adds a listener and calls it on event', function () {
-			var listener1 = sinon.spy(),
-			    listener2 = sinon.spy();
+			let listener1 = sinon.spy(),
+			    listener2 = sinon.spy()
 
-			L.DomEvent.addListener(el, 'click', listener1);
-			L.DomEvent.addListener(el, 'click', listener2);
+			DomEvent.addListener(el, 'click', listener1)
+			DomEvent.addListener(el, 'click', listener2)
 
-			simulateClick(el);
+			simulateClick(el)
 
-			expect(listener1.called).to.be.ok();
-			expect(listener2.called).to.be.ok();
-		});
+			expect(listener1.called).to.be.ok()
+			expect(listener2.called).to.be.ok()
+		})
 
 		it('binds "this" to the given context', function () {
-			var obj = {foo: 'bar'},
-			    result;
+			let obj = {foo: 'bar'},
+			    result
 
-			L.DomEvent.addListener(el, 'click', function () {
-				result = this;
-			}, obj);
+			DomEvent.addListener(el, 'click', function () {
+				result = this
+			}, obj)
 
-			simulateClick(el);
+			simulateClick(el)
 
-			expect(result).to.eql(obj);
-		});
+			expect(result).to.eql(obj)
+		})
 
 		it('passes an event object to the listener', function () {
-			var type;
+			let type
 
-			L.DomEvent.addListener(el, 'click', function (e) {
-				type = e && e.type;
-			});
-			simulateClick(el);
+			DomEvent.addListener(el, 'click', function (e) {
+				type = e && e.type
+			})
+			simulateClick(el)
 
-			expect(type).to.eql('click');
-		});
-	});
+			expect(type).to.eql('click')
+		})
+	})
 
 	describe('#removeListener', function () {
 		it('removes a previously added listener', function () {
-			var listener = sinon.spy();
+			let listener = sinon.spy()
 
-			L.DomEvent.addListener(el, 'click', listener);
-			L.DomEvent.removeListener(el, 'click', listener);
+			DomEvent.addListener(el, 'click', listener)
+			DomEvent.removeListener(el, 'click', listener)
 
-			simulateClick(el);
+			simulateClick(el)
 
-			expect(listener.called).to.not.be.ok();
-		});
-	});
+			expect(listener.called).to.not.be.ok()
+		})
+	})
 
 	describe('#stopPropagation', function () {
 		it('stops propagation of the given event', function () {
-			var child = document.createElement('div'),
-			    listener = sinon.spy();
+			let child = document.createElement('div'),
+			    listener = sinon.spy()
 
-			el.appendChild(child);
+			el.appendChild(child)
 
-			L.DomEvent.addListener(child, 'click', L.DomEvent.stopPropagation);
-			L.DomEvent.addListener(el, 'click', listener);
+			DomEvent.addListener(child, 'click', DomEvent.stopPropagation)
+			DomEvent.addListener(el, 'click', listener)
 
-			simulateClick(child);
+			simulateClick(child)
 
-			expect(listener.called).to.not.be.ok();
+			expect(listener.called).to.not.be.ok()
 
-			el.removeChild(child);
-		});
-	});
+			el.removeChild(child)
+		})
+	})
 	describe('#preventDefault', function () {
 		it('prevents the default action of event', function () {
-			L.DomEvent.addListener(el, 'click', L.DomEvent.preventDefault);
+			DomEvent.addListener(el, 'click', DomEvent.preventDefault)
 
-			expect(simulateClick(el)).to.be(false);
-		});
-	});
-});
+			expect(simulateClick(el)).to.be(false)
+		})
+	})
+})
