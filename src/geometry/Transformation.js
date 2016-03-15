bdf5699a74fe39fc4ced0f3a9001bbcc9c4f25d2
Stephen Moore <stephen.moore@linux.com>
2016-03-15 12:14:38 -0700

    removed semicolons from Transformation.js

--- src/geometry/Transformation.js
+++ src/geometry/Transformation.js
@@ -1,33 +1,33 @@
 /*
  * L.Transformation is an utility class to perform simple point transformations through a 2d-matrix.
  */
-import {Point} from './Point';
+import {Point} from './Point'
 
 export class Transformation {
 
 	constructor(a, b, c, d) {
-		this._a = a;
-		this._b = b;
-		this._c = c;
-		this._d = d;
+		this._a = a
+		this._b = b
+		this._c = c
+		this._d = d
 	}
 
 	transform(point, scale) { // (Point, Number) -> Point
-		return this._transform(point.clone(), scale);
+		return this._transform(point.clone(), scale)
 	}
 
 	// destructive transform (faster)
 	_transform(point, scale) {
-		scale = scale || 1;
-		point.x = scale * (this._a * point.x + this._b);
-		point.y = scale * (this._c * point.y + this._d);
-		return point;
+		scale = scale || 1
+		point.x = scale * (this._a * point.x + this._b)
+		point.y = scale * (this._c * point.y + this._d)
+		return point
 	}
 
 	untransform(point, scale) {
-		scale = scale || 1;
+		scale = scale || 1
 		return new Point(
 		        (point.x / scale - this._b) / this._a,
-		        (point.y / scale - this._d) / this._c);
+		        (point.y / scale - this._d) / this._c)
 	}
 }
